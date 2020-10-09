c2374d04c336cbb309f6d2bf6e32903736b1b065
dazelpro <dazelpro@gmail.com>
2020-10-09 10:54:26 +0700

    + fix bug css api

--- js/scroll.js
+++ js/scroll.js
@@ -1,27 +1,27 @@
 $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
     if (scroll >= 100) {
-        $(".link1").addClass("active");
-        $(".link2").removeClass("active");
-        $(".link3").removeClass("active");
-        $(".link4").removeClass("active");
+        $(".link1").addClass("actives");
+        $(".link2").removeClass("actives");
+        $(".link3").removeClass("actives");
+        $(".link4").removeClass("actives");
     }
     if (scroll >= 1100) {
-        $(".link2").addClass("active");
-        $(".link1").removeClass("active");
-        $(".link3").removeClass("active");
-        $(".link4").removeClass("active");
+        $(".link2").addClass("actives");
+        $(".link1").removeClass("actives");
+        $(".link3").removeClass("actives");
+        $(".link4").removeClass("actives");
     }
     if (scroll >= 1600) {
-        $(".link3").addClass("active");
-        $(".link1").removeClass("active");
-        $(".link2").removeClass("active");
-        $(".link4").removeClass("active");
+        $(".link3").addClass("actives");
+        $(".link1").removeClass("actives");
+        $(".link2").removeClass("actives");
+        $(".link4").removeClass("actives");
     }
     if (scroll >= 2200) {
-        $(".link4").addClass("active");
-        $(".link1").removeClass("active");
-        $(".link2").removeClass("active");
-        $(".link3").removeClass("active");
+        $(".link4").addClass("actives");
+        $(".link1").removeClass("actives");
+        $(".link2").removeClass("actives");
+        $(".link3").removeClass("actives");
     }
 });
