6fae9ff22fa03349a4b4f3b5cc3b56144fc5fe85
dazelpro <dazelpro@gmail.com>
2021-06-15 20:26:29 +0700

    privacy police facebook api

--- dark/js/ig.js
+++ dark/js/ig.js
@@ -1,16 +1,16 @@
 function checkURL() {
     let urlIG = document.getElementById("urlig").value;
     let tknFB = document.getElementById("token").value;
-    let url = `https://graph.facebook.com/v9.0/instagram_oembed?url=${urlIG}&maxwidth=320&access_token=${tknFB}`
+    let url = `https://graph.facebook.com/v11.0/instagram_oembed?url=${urlIG}&maxwidth=320&access_token=${tknFB}`
     if (urlIG !== '') {
         document.getElementById("after-loading").style.display = "none";
         document.getElementById("get-loading").style.display = "block";
-        (async () => {
+        (async() => {
             const rawResponse = await fetch(url, {
                 method: 'GET'
             });
             const content = await rawResponse.json();
-            if(content['author_name'] !== undefined){
+            if (content['author_name'] !== undefined) {
                 document.getElementById("get-loading").style.display = "none";
                 document.getElementById("after-loading").style.display = "block";
                 document.getElementById("image-thumb").innerHTML = `<img style="width: 250px;" src="${content['thumbnail_url']}" alt="">`;
@@ -19,7 +19,7 @@ function checkURL() {
                 document.getElementById("code2").innerHTML = `&lt;img src="${content['thumbnail_url']}"&gt;`;
             } else {
                 document.getElementById("get-loading").style.display = "none";
-                alert("There is something wrong!")
+                alert("Kebijakan pemakaian Facebook API telah berubah. Kunjungi halaman Facebook Developers untuk informasi lebih lanjut.")
             }
         })();
     } else {
